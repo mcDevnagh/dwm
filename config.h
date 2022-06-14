7bced4b03f6b5736e976a076e4b5beaaf136c4c7
Michael McDonagh <michael@mcdonagh.dev>
2022-06-13 21:12:02 -0400

    Added support for emojs with libxft-bgra

--- config.h
+++ config.h
@@ -8,7 +8,10 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Hack Nerd Font Mono:monospace:size=16" };
+static const char *fonts[] = {
+    "Hack Nerd Font Mono:monospace:size=16",
+    "JoyPixels:pixelsize=14:antialias=true:autohint=true"
+};
 static const char dmenufont[]       = "monospace:size=16";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
